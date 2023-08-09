2f1ff27512d4e83301acb1fed7c3a0e4ec347e5c
Daisy Reis <90834386+DaisyCR@users.noreply.github.com>
2023-08-09 05:14:10 -0300

    Add animation on scroll

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-let navbar = document.getElementById("navbar");
-let sticky = navbar.offsetTop;
+const navbar = document.getElementById("navbar");
+const sticky = navbar.offsetTop;
+const hiddenElements = document.querySelectorAll(".hidden");
 
 //Add class to keep navbar stuck on top after scrolling past it
 window.onscroll = () => {
-  (window.scrollY >= sticky) ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
-}
+  window.scrollY >= sticky
+    ? navbar.classList.add("sticky")
+    : navbar.classList.remove("sticky");
+};
 
 //Return to the start of the page
-backToTop = () => {
+const backToTop = () => {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+};
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    entry.isIntersecting
+      ? entry.target.classList.add("show")
+      : entry.target.classList.remove("show");
+  });
+});
+hiddenElements.forEach((e) => observer.observe(e));
