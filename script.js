d625b3f4386568d50af5ad835010929223fe3889
Daisy Reis <90834386+DaisyCR@users.noreply.github.com>
2023-08-09 01:04:04 -0300

    Second Commit

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 window.onscroll = function() {myFunction()};
 
 var navbar = document.getElementById("nav-bar");
-var sticky = navbar.offsetTop+770;
+var sticky = navbar.offsetTop;
+let mybutton = document.getElementById("back-to-top-btn");
 
 function myFunction() {
     if (window.scrollY >= sticky) {
@@ -9,4 +10,9 @@ function myFunction() {
     } else {
       navbar.classList.remove("sticky");
     }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
